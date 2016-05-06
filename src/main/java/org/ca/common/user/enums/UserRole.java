0df90abc4684717e1b1a19029b864e86c5b8d3b5
ligson <lijinsheng@lecxe.com>
2016-05-06 14:59:39 +0800

    增加用户角色

---
+++ src/main/java/org/ca/common/user/enums/UserRole.java
@@ -0,0 +1,23 @@
+package org.ca.common.user.enums;
+
+/**
+ * Created by ligson on 2016/5/6.
+ */
+public enum UserRole {
+    USER(1, "普通用户"), RA_ADMIN(2, "RA管理员"), CA_ADMIN(3, "CA管理员"), SUPER(100, "超级管理员");
+    private int code;
+    private String msg;
+
+    UserRole(int code, String msg) {
+        this.code = code;
+        this.msg = msg;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+}
