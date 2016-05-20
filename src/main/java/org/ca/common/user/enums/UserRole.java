1f015839fd5a05c75f5337514ac458c62a966f1f
ligson <lijinsheng@lecxe.com>
2016-05-20 10:17:54 +0800

    增加离线CA管理员

--- src/main/java/org/ca/common/user/enums/UserRole.java
+++ src/main/java/org/ca/common/user/enums/UserRole.java
@@ -4,7 +4,7 @@ package org.ca.common.user.enums;
  * Created by ligson on 2016/5/6.
  */
 public enum UserRole {
-    USER(1, "普通用户"), RA_ADMIN(2, "RA管理员"), CA_ADMIN(3, "CA管理员"), SUPER(100, "超级管理员");
+    USER(1, "普通用户"), RA_ADMIN(2, "RA管理员"), CA_ADMIN(3, "CA管理员"), OFFLINE_ADMIN(4, "离线CA管理员"), SUPER(100, "超级管理员");
     private int code;
     private String msg;
 
